2349ae174b279192c8f4bb97f84eac0a88ece5f8
AgcaoiliAdrian <72775271+AgcaoiliAdrian@users.noreply.github.com>
2021-06-05 01:32:30 +0800

    Update connection.inc.php

--- connection.inc.php
+++ connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","ecommerce");
+$con=mysqli_connect(" remotemysql.com","rQfEG8iyLP","jCH9v6IK8m","rQfEG8iyLP");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Coffee Hour');
 define('SITE_PATH','http://localhost/Coffee%20Hour/');
 
